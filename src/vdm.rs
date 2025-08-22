5e36f908e41c0eea2d3f8a8c57a7e432d614d7be
zhuyi2024 <79184862@qq.com>
2025-08-22 09:37:38 +0800

    Update src/vdm.rs
    Co-authored-by: Adam Sasine <adam.sasine@gmail.com>

--- src/vdm.rs
+++ src/vdm.rs
@@ -1,14 +1,14 @@
-pub const PD_VDM_DATA_OBJ_SIZE: usize = 4;
-pub const PD_VDM_MAX_VDOS: usize = 6;
-pub const PD_VDM_MAX_NUM_DATA_OBJECTS: usize = 7;
-pub const PD_VDM_MAX_SIZE: usize = PD_VDM_DATA_OBJ_SIZE * PD_VDM_MAX_NUM_DATA_OBJECTS;
-pub const PD_VDM_OBJ_POS_ALL_MODES: u8 = 7;
-pub const PD_VDM_HEADER_VERSION: u8 = 1;
+pub const DATA_OBJ_SIZE: usize = 4;
+pub const MAX_VDOS: usize = 6;
+pub const MAX_NUM_DATA_OBJECTS: usize = 7;
+pub const MAX_SIZE: usize = DATA_OBJ_SIZE * MAX_NUM_DATA_OBJECTS;
+pub const OBJ_POS_ALL_MODES: u8 = 7;
+pub const HEADER_VERSION: u8 = 1;
 
 /// SVDM header commands
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum VdmCmd {
+pub enum Cmd {
     /// Discover Id
     DiscId = 1,
     /// Discover SVIDs
