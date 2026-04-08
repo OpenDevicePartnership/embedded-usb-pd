7370000615d8321728774f559aeeeabf070696c6
Adam Sasine <asasine@users.noreply.github.com>
2026-04-08 13:30:47 -0700

    Remove USB4 SVID

--- src/vdm/svid.rs
+++ src/vdm/svid.rs
@@ -6,6 +6,5 @@ pub struct Svid(pub u16);
 impl Svid {
     pub const PD: Self = Self(0xFF00);
     pub const DISPLAY_PORT_TYPE_C: Self = Self(0xFF01);
-    pub const USB4: Self = Self(0xFF03);
     pub const THUNDERBOLT: Self = Self(0x8087);
 }
