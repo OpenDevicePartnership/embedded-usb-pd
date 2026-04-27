8aad60a2669d9c387d713b3ae4313f7f8040c85c
Adam Sasine <asasine@users.noreply.github.com>
2026-04-27 10:37:17 -0700

    Fix jjmn implementation and add tests

--- src/usb.rs
+++ src/usb.rs
@@ -13,9 +13,9 @@ impl Bcd {
     /// - `m` is the minor version (1 nibble)
     /// - `n` is the subminor version (1 nibble)
     pub const fn jjmn(&self) -> (u8, u8, u8) {
-        let jj = (self.0 >> 12) as u8;
-        let m = ((self.0 >> 8) & 0xF) as u8;
-        let n = ((self.0 >> 4) & 0xF) as u8;
+        let jj = (self.0 >> 8) as u8;
+        let m = ((self.0 >> 4) & 0xF) as u8;
+        let n = (self.0 & 0xF) as u8;
         (jj, m, n)
     }
 }
@@ -24,3 +24,14 @@ impl Bcd {
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct ProductId(pub u16);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bcd_jjmn() {
+        assert_eq!(Bcd(0x1234).jjmn(), (0x12, 0x3, 0x4));
+        assert_eq!(Bcd(0xFEDC).jjmn(), (0xFE, 0xD, 0xC));
+    }
+}
